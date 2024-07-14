fa4a1fecb48b87d700bedfe272e82b6619aa3a34
deekshithgodha <228t1a4240@diet.ac.in>
2024-07-14 17:20:48 +0530

    Update image.py

--- image.py
+++ image.py
@@ -5,7 +5,7 @@ from transformers import BlipProcessor, BlipForConditionalGeneration
 processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
 model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
 
-img_url ='C:\Users\deeks\Downloads\nanipuppydownload.jpg'
+img_url = 'https://images.unsplash.com/photo-1518378188025-22bd89516ee2?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
 raw_image = Image.open(requests.get(img_url, stream=True).raw).convert('RGB')
 
 # conditional image captioning
